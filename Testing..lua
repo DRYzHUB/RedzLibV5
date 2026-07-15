190644fa3a1612c523757b79844d8321b572737f
DRYzHUB <kaelix196@gmail.com>
2026-07-15 10:08:02 -0300

    Add files via upload

--- Testing..lua
+++ Testing..lua
@@ -610,21 +610,9 @@ function redzlib:MakeWindow(Configs)
 	local MainFrame = InsertTheme(Create("ImageButton", ScreenGui, {
 		Size = UDim2.fromOffset(UISizeX, UISizeY),
 		Position = UDim2.new(0.5, -UISizeX/2, 0.5, -UISizeY/2),
-		BackgroundTransparency = 1,
+		BackgroundTransparency = 0.03,
 		Name = "Hub"
 	}), "Main")
-
-    local WindowBackground = Create("ImageLabel", MainFrame, {
-        Name = "WindowBackground",
-        Size = UDim2.new(1, 0, 1, 0),
-        Position = UDim2.new(0, 0, 0, 0),
-        BackgroundTransparency = 1,
-        Image = "rbxassetid://133021256665611", 
-        ImageTransparency = 0.03,
-        ScaleType = Enum.ScaleType.Crop,
-        ZIndex = 0
-    })
-    Make("Corner", WindowBackground)
 
 	Make("Gradient", MainFrame, {
 		Rotation = 45
